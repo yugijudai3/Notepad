02c5be4cc23c37b548987a65731fc15d7ffc5e46
yugijudai3 <yugiohocg123@gmail.com>
2019-05-17 01:20:31 +0900

    memo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { sanitizeHtml } from '@angular/core/src/sanitization/sanitization';
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  post: {
+    message: string
+  }[]=[{
+    message: "〇〇君へ折り返しの電話"
+  },{
+    message: "来週の打ち合わせ"
+  }];
 }
